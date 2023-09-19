6145c1673406b6f51f9e456ddd71a3fefbcfefd6
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2023-09-18 22:23:07 -0400

    Nvim: get telescope to work

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -9,7 +9,14 @@ return {
     config = function()
       require("nvim-tree").setup {}
     end,
-  }
+  },
+  {
+    'nvim-telescope/telescope.nvim', tag = '0.1.3',
+    dependencies = { 'nvim-lua/plenary.nvim' }
+  },
+  {
+    'nvim-telescope/telescope-fzf-native.nvim', build = 'make'
+  },
   -- "folke/neodev.nvim",
   -- "folke/which-key.nvim",
   -- { "folke/neoconf.nvim", cmd = "Neoconf" },
