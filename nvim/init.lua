1ad3cd8ef14de6c61d6b45887c0b0cf3c6b25467
Tony Distinti <25881824+breadoliveoilsalt@users.noreply.github.com>
2024-07-03 15:08:03 -0400

    Nvim: Move telescope settings to plugin file

--- nvim/init.lua
+++ nvim/init.lua
@@ -287,9 +287,9 @@ vim.opt.autoread = true
 -- Triger `autoread` when files changes on disk
 -- See:
 --  * :help checktime
---   * https://stackoverflow.com/questions/923737/detect-file-change-offer-to-reload-file
---   * https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim
---     * NOTE with above: it requires a tmux conf change
+--  * https://stackoverflow.com/questions/923737/detect-file-change-offer-to-reload-file
+--  * https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim
+--    * NOTE with above: it requires a tmux conf change
 vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained", "BufEnter" }, {
 	pattern = { "*" },
 	command = "checktime",
@@ -300,97 +300,6 @@ vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI", "FocusGained", "BufEn
 vim.api.nvim_create_autocmd("FileChangedShellPost", {
 	pattern = "*",
 	command = "echohl WarningMsg | echo 'File changed on disk. Buffer reloaded.' | echohl None",
-})
-
--- Probably redundant of above. Consider deleting after
--- wait period
--- vim.api.nvim_create_autocmd(
---   "FileChangedShell",
---   {
---     pattern = { "*" },
---     callback = function() print("TN Warning: File changed on disk") end
---   }
--- )
-
----------------
--- TELESCOPE --
----------------
-
-local telescope = require("telescope")
-
-telescope.load_extension("fzf")
-
-local find_file_command = {
-	"rg",
-	"--files",
-	"--hidden",
-	"-g",
-	"!.git",
-	"-g",
-	"!**/*/PackageBuilder/resources.js",
-	"-g",
-	"!**/*/src/assets/",
-	"-g",
-	"!*.snap",
-	"-g",
-	"!build/",
-}
-
-telescope.setup({
-	defaults = {
-		layout_config = { height = 0.98, width = 0.98, preview_width = 0.6 },
-		vimgrep_arguments = {
-			-- set grepprg=rg\ --hidden\ --follow\ --vimgrep
-			-- nnoremap <Leader>fw :grep! -g "!**/*/PackageBuilder/resources.js" -g "!**/*/src/assets/" -g "!.git/" -g "!*.snap" -g "!build/" -e '
-
-			"rg",
-			"--hidden",
-			"--color=never",
-			"--no-heading",
-			"--with-filename",
-			"--line-number",
-			"--column",
-			"--smart-case",
-			"--glob=!**/*/src/assets/",
-			"--glob=!.git/",
-			"--glob=!*.snap",
-			-- BEWARE
-			-- "--glob=!*.test.*",
-		},
-		-- Preserve past searches
-		-- See: https://github.com/nvim-telescope/telescope.nvim/issues/2024
-		-- :help telescope.defaults.cache_picker
-		cache_picker = {
-			num_pickers = 5,
-		},
-	},
-	pickers = {
-		find_files = {
-			find_command = find_file_command,
-		},
-	},
-})
-
-local telescope_builtin = require("telescope.builtin")
-
-vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
-vim.keymap.set("n", "<Leader>fw", telescope_builtin.live_grep, {})
--- BIG LESSON / REMINDER HERE: the third argument has to be a function.
--- If you are using options, make sure you wrap things in
--- anonymous function.
--- vim.keymap.set("n", "<C-p>", function() telescope_builtin.buffers({ sort_lastused = true, ignore_current_buffer = true }) end)
-vim.keymap.set("n", "<C-p>", function()
-	telescope_builtin.buffers({ sort_lastused = true })
-end)
-vim.keymap.set("n", "<Leader>fb", function()
-	telescope_builtin.buffers({ sort_lastused = true, ignore_current_buffer = true })
-end)
-vim.keymap.set("n", "<Leader>fh", telescope_builtin.help_tags, {})
-vim.keymap.set("n", "<Leader>fl", telescope_builtin.resume, {
-	desc = "[f]ind [l]ast: bring up last search auto-[c]ommenting",
-})
-vim.keymap.set("n", "<Leader>fc", telescope_builtin.pickers, {
-	desc = "[f]ind [c]ached: bring up picker for last several searches",
 })
 
 -------------
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,8 +1,89 @@
+local setup = function()
+	local telescope = require("telescope")
+
+	telescope.load_extension("fzf")
+
+	local find_file_command = {
+		"rg",
+		"--files",
+		"--hidden",
+		"-g",
+		"!.git",
+		"-g",
+		"!**/*/PackageBuilder/resources.js",
+		"-g",
+		"!**/*/src/assets/",
+		"-g",
+		"!*.snap",
+		"-g",
+		"!build/",
+	}
+
+	telescope.setup({
+		defaults = {
+			layout_config = { height = 0.98, width = 0.98, preview_width = 0.6 },
+			vimgrep_arguments = {
+				-- set grepprg=rg\ --hidden\ --follow\ --vimgrep
+				-- nnoremap <Leader>fw :grep! -g "!**/*/PackageBuilder/resources.js" -g "!**/*/src/assets/" -g "!.git/" -g "!*.snap" -g "!build/" -e '
+
+				"rg",
+				"--hidden",
+				"--color=never",
+				"--no-heading",
+				"--with-filename",
+				"--line-number",
+				"--column",
+				"--smart-case",
+				"--glob=!**/*/src/assets/",
+				"--glob=!.git/",
+				"--glob=!*.snap",
+				-- BEWARE
+				-- "--glob=!*.test.*",
+			},
+			-- Preserve past searches
+			-- See: https://github.com/nvim-telescope/telescope.nvim/issues/2024
+			-- :help telescope.defaults.cache_picker
+			cache_picker = {
+				num_pickers = 5,
+			},
+		},
+		pickers = {
+			find_files = {
+				find_command = find_file_command,
+			},
+		},
+	})
+
+	local telescope_builtin = require("telescope.builtin")
+
+	vim.keymap.set("n", "<leader>ff", telescope_builtin.find_files, {})
+	vim.keymap.set("n", "<Leader>fw", telescope_builtin.live_grep, {})
+
+	-- BIG LESSON / REMINDER HERE: the third argument has to be a function.
+	-- If you are using options, make sure you wrap things in an
+	-- anonymous function.
+	-- Below, another potential option to consider: { ignore_current_buffer = true }
+	vim.keymap.set("n", "<C-p>", function()
+		telescope_builtin.buffers({ sort_lastused = true })
+	end)
+	vim.keymap.set("n", "<Leader>fb", function()
+		telescope_builtin.buffers({ sort_lastused = true, ignore_current_buffer = true })
+	end)
+	vim.keymap.set("n", "<Leader>fh", telescope_builtin.help_tags, {})
+	vim.keymap.set("n", "<Leader>fl", telescope_builtin.resume, {
+		desc = "[f]ind [l]ast: bring up last search auto-[c]ommenting",
+	})
+	vim.keymap.set("n", "<Leader>fc", telescope_builtin.pickers, {
+		desc = "[f]ind [c]ached: bring up picker for last several searches",
+	})
+end
+
 return {
 	{
 		"nvim-telescope/telescope.nvim",
 		tag = "0.1.3",
 		dependencies = { "nvim-lua/plenary.nvim" },
+		config = setup,
 	},
 	{
 		"nvim-telescope/telescope-fzf-native.nvim",
